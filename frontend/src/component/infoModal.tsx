c66b486fa51576758b8fe18f877f50b7f8500ba6
noodaj <jadon.loi.68@gmail.com>
2023-04-08 00:35:40 -0700

    info details

--- frontend/src/component/infoModal.tsx
+++ frontend/src/component/infoModal.tsx
@@ -11,12 +11,27 @@ export const InfoModal: FC<InfoProps> = ({ modalState, setModal }) => {
 			<div className="w-[500px] h-[600px] border-1 rounded-lg bg-[#0e0f10]">
 				<nav>
 					<IoMdClose
-						className="hover:cursor-pointer"
+						className="float-right mr-4 mt-4 hover:cursor-pointer"
 						onClick={() => {
 							setModal(!modalState);
 						}}
 					></IoMdClose>
 				</nav>
+				<div className="mx-6 mt-16 text-lg">
+					<div className="pb-3">
+						<h2 className="text-2xl">How To Play</h2>
+						<h3 className="text-lg font-semibold">
+							Guess the Wordle in 6 tries.
+						</h3>
+					</div>
+					<div className="font-normal text-base">
+						<li>Each guess must be a valid 5-letter word.</li>
+						<li>
+							The color of the tiles will change to show how close
+							your guess was to the word.
+						</li>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
