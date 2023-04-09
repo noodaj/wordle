663296c0762ee6ae07589bd9b9adcb1ef6eb3900
noodaj <jadon.loi.68@gmail.com>
2023-04-09 00:45:03 -0700

    logic rework

--- frontend/src/component/cell.tsx
+++ frontend/src/component/cell.tsx
@@ -10,20 +10,21 @@ interface CellProps {
 export const Cell: FC<CellProps> = ({ letter, r, c }) => {
 	const { index, actualWord } = useContext(BoardContext);
 
-	const correctLetter = letter != "" && letter == actualWord[c];
+	const correctLetter = letter != "" && letter === actualWord[c];
 	const hasLetter = letter != "" && actualWord.includes(letter);
 	let color = "";
 	if (index.row > r) {
-		if (hasLetter) {
+		if (hasLetter && !correctLetter) {
 			color = "bg-[#B59F3B]";
 		}
-		if (correctLetter) {
+		if (correctLetter && hasLetter) {
 			color = "bg-[#538D4E]";
 		}
 		if(!correctLetter && !hasLetter && letter != ""){
 			color = "bg-[#3A3A3C]"
 		}
 	}
+
 	return (
 		<>
 			<div className={`flex items-center justify-center border-2 text-3xl font-semibold w-16 h-16 mx-1 ${color}`}>
