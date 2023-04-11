e02a44a6005c746284c027777536ff448a937fa9
noodaj <jadon.loi.68@gmail.com>
2023-04-11 00:05:47 -0700

    getting daily/checking word

--- backend/src/routes/word.server.ts
+++ backend/src/routes/word.server.ts
@@ -4,7 +4,7 @@ import { word } from "../util/types.server";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-	const dailyWord = req.app.locals.dailyWord.toUpperCase();
+	const dailyWord = req.app.locals.dailyWord;
 
 	res.status(200).json({ daily: dailyWord });
 });
@@ -15,17 +15,17 @@ router.post("/checkWord", (req: Request, res: Response) => {
 	const validWord = req.app.locals.dailyWord;
 
 	if (!wordSet.has(word)) {
-        console.log("invalid word")
-		return res.status(400).json({ message: "Invalid word" });
+		console.log("invalid word");
+		return res.status(400).send({ error: "Invalid word" });
 	}
 
 	if (word === validWord) {
-        console.log("correct word")
-		return res.status(200).json({ message: "Correct guess" });
+		console.log("correct word");
+		return res.status(200).send({ message: "Correct guess" });
 	}
 
-    console.log("wrong word")
-	return res.status(400).json({ message: "Wrong word" });
+	console.log("wrong word");
+	return res.status(200).send({ error: "Wrong word" });
 });
 
 export { router as wordRouter };
