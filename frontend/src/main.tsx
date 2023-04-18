3978d3114515996178285956adcfa209d6af067d
noodaj <jadon.loi.68@gmail.com>
2023-04-18 00:24:54 -0700

    Add: use react query to get data from backend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { disableReactDevTools } from "./util/disableDevTools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 if (process.env.NODE_ENV === "production") disableReactDevTools();
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>
 );
