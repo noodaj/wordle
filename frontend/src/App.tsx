37ddd09abb2fd74c6536ef08346012fdb2bf8874
noodaj <jadon.loi.68@gmail.com>
2023-04-22 22:59:09 -0700

    refresh to save stats

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { createContext, useEffect, useState } from "react";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import React, { createContext, useEffect, useRef, useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { AnimatePresence } from "framer-motion";
 import { useCookies } from "react-cookie";
 import { Board } from "./component/board";
@@ -18,29 +18,41 @@ export const BoardContext = createContext<AppContext>({
 	index: { row: 0, col: 0 },
 	login: false,
 	actualWord: "",
-	curGuess: "",
 	showLogin: () => undefined,
 	setIndex: () => undefined,
-	setGuess: () => undefined,
+	setBoard: () => undefined,
 	enterKey: () => undefined,
 	backKey: () => undefined,
 	letterKey: () => undefined,
 });
 
+//function to get userData if user is signed in
+const fetchData = async (userID: string) => {
+	try {
+		const res = await axios.get("http://localhost:5000/user/getData", {
+			params: { userID: userID },
+		});
+		const data = await res.data;
+		return data;
+	} catch (err) {
+		console.log(err);
+	}
+};
+
 function App() {
 	const [cookie, _] = useCookies(["auth_token"]);
-	const queryClient = useQueryClient();
 	const [board, setBoard] = useState<string[][]>(boardGrid);
-	const [actualWord, setWord] = useState<string>(" ");
-	const [curGuess, setGuess] = useState<string>("");
 	const [stats, showStats] = useState<boolean>(false);
 	const [login, setLogin] = useState<boolean>(false);
 	const [infoState, setInfoState] = useState<boolean>(false);
 	const [invalidWord, setInvalid] = useState<boolean>(false);
 	const [sidePanel, showSidePanel] = useState<boolean>(false);
-	const [guessCount, setGuessCount] = useState<number>(0);
 	const [index, setIndex] = useState<curGuess>({ row: 0, col: 0 });
-	const [userStats, setUserStats] = useState<userStats>({
+
+	//REFS?
+	const actualWord = useRef<string | null>(null);
+	const curGuess = useRef<string | null>("");
+	const userStats = useRef<userStats>({
 		curStreak: 0,
 		maxStreak: 0,
 		played: 0,
@@ -49,10 +61,11 @@ function App() {
 		distribution: [0, 0, 0, 0, 0, 0],
 	});
 
+	//might be better to just have a useeffect checking if the there is a cookie
 	useQuery({
 		queryKey: ["fetch"],
 		queryFn: () => {
-			if (cookie.auth_token != undefined) {
+			if (cookie.auth_token) {
 				const userID = window.localStorage.getItem("userID");
 				const res = fetchData(userID!);
 				return res;
@@ -61,7 +74,9 @@ function App() {
 		},
 		onSuccess: (data) => {
 			if (data) {
-				setUserStats(data);
+				userStats.current = data;
+				setBoard(data.game.board);
+				setIndex(data.game.index);
 			}
 		},
 	});
@@ -72,12 +87,13 @@ function App() {
 				{
 					userID: userID,
 					stats: {
-						wins: 1,
-						curStreak: 1,
-						maxStreak: 11,
-						winPercent: 5,
-						played: 1,
-						distribution: [],
+						wins: userStats.current.wins,
+						curStreak: userStats.current.curStreak,
+						maxStreak: userStats.current.maxStreak,
+						winPercent: userStats.current.winPercent,
+						played: userStats.current.played,
+						distribution: userStats.current.distribution,
+						game: { board: board, index: index, guess: curGuess },
 					},
 				},
 			]),
@@ -86,7 +102,7 @@ function App() {
 	const keyEnter = () => {
 		if (index.col !== 5) return;
 
-		if (!checkWord(curGuess.toLowerCase())) {
+		if (!checkWord(curGuess.current!.toLowerCase())) {
 			setInvalid(true);
 			setTimeout(() => {
 				setInvalid(false);
@@ -103,7 +119,7 @@ function App() {
 		temp[index.row][index.col - 1] = "";
 		setBoard(temp);
 		setIndex({ ...index, col: index.col >= 0 ? index.col - 1 : 0 });
-		setGuess(board[index.row].join(""));
+		curGuess.current = board[index.row].join("");
 	};
 
 	const letterKey = (letter: string) => {
@@ -114,20 +130,7 @@ function App() {
 			temp[index.row][index.col] = letter;
 			setBoard(temp);
 			setIndex({ ...index, col: index.col + 1 });
-			setGuess(board[index.row].join(""));
-		}
-	};
-
-	//function to get userData if user is signed in
-	const fetchData = async (userID: string) => {
-		try {
-			const res = await axios.get("http://localhost:5000/user/getData", {
-				params: { userID: userID },
-			});
-			const data = await res.data;
-			return data;
-		} catch (err) {
-			console.log(err);
+			curGuess.current = board[index.row].join("");
 		}
 	};
 
@@ -135,24 +138,24 @@ function App() {
 	useEffect(() => {
 		//get daily word
 		const newDaily = getDaily();
-		setWord(newDaily.toUpperCase());
+		actualWord.current = newDaily.toUpperCase();
 
 		//if there is no token, we get data from local storage
-		if (!cookie.auth_token) {
+		if (cookie.auth_token === undefined) {
 			const curGameData = window.localStorage.getItem("currentGame");
 			const curUserData = window.localStorage.getItem("userStats");
 
 			//parse local user stats and board
 			if (curUserData) {
 				const res: userStats = JSON.parse(curUserData);
-				setUserStats({
+				userStats.current = {
 					curStreak: res.curStreak,
 					maxStreak: res.maxStreak,
 					winPercent: res.winPercent,
 					wins: res.wins,
 					played: res.played,
 					distribution: res.distribution,
-				});
+				};
 			}
 
 			if (curGameData) {
@@ -161,6 +164,16 @@ function App() {
 				setIndex(curGame.index);
 			}
 		}
+
+		return () => {
+			if (cookie.auth_token !== undefined) {
+				window.addEventListener("beforeunload", (e) => {
+					e.preventDefault();
+					const userID = window.localStorage.getItem("userID");
+					update.mutate(userID!);
+				});
+			}
+		};
 	}, []);
 
 	//use effect to run whenever the index changes
@@ -169,39 +182,49 @@ function App() {
 			const curGame: currentGame = {
 				board,
 				index: { row: index.row, col: index.col },
-				guess: curGuess,
+				guess: curGuess.current!,
 			};
 			window.localStorage.setItem("currentGame", JSON.stringify(curGame));
 		}
-	}, [index]);
 
-	if (curGuess === actualWord || index.row == 6) {
-		let win: number = userStats.wins;
-		let played: number = userStats.played + 1;
-		let newDistribution: number[] = userStats.distribution;
-		if (curGuess === actualWord) {
-			setGuess(() => "");
-			newDistribution[index.row] += 1 / 2;
-			setGuessCount(index.col + 1);
-			win += 1;
-		}
-		setIndex({ row: 5, col: 6 });
+		if (curGuess.current === actualWord.current || index.row == 6) {
+			console.log(curGuess.current, actualWord.current);
+			let win: number = userStats.current.wins;
+			let played: number = userStats.current.played + 1;
+			let newDistribution: number[] = userStats.current.distribution;
+			if (curGuess.current === actualWord.current) {
+				curGuess.current = "";
 
-		const newStats: userStats = {
-			curStreak: userStats.curStreak,
-			played: played,
-			wins: win,
-			winPercent: (win / played) * 100,
-			maxStreak: userStats.maxStreak,
-			distribution: newDistribution,
-		};
+				//remove /2 in production 
+				//happens bc of strict mode
+				newDistribution[index.row] += 1 / 2;
+				win += 1;
+			}
+			setIndex({ row: 5, col: 6 });
 
-		if (!cookie.auth_token) {
-			setUserStats(newStats);
+			const newStats: userStats = {
+				curStreak: userStats.current.curStreak,
+				played: played,
+				wins: win,
+				winPercent: (win / played) * 100,
+				maxStreak: userStats.current.maxStreak,
+				distribution: newDistribution,
+			};
+
+			if (!cookie.auth_token) {
+				window.localStorage.setItem(
+					"userStats",
+					JSON.stringify(newStats)
+				);
+			} else {
+				const userID = window.localStorage.getItem("userID");
+				update.mutate(userID!);
+			}
+
+			userStats.current = newStats;
 			showStats(true);
-			window.localStorage.setItem("userStats", JSON.stringify(newStats));
 		}
-	}
+	}, [index]);
 
 	return (
 		<div className="App min-h-screen overflow-hidden bg-[#0e0f10] py-3 font-sans text-white">
@@ -221,10 +244,12 @@ function App() {
 			<AnimatePresence initial={false} mode="wait">
 				{sidePanel && (
 					<SidePanel
+						setUserStats={userStats}
 						sidePanel={sidePanel}
 						showSidePanel={showSidePanel}
 						login={login}
 						showLogin={setLogin}
+						curGuess={curGuess}
 					></SidePanel>
 				)}
 			</AnimatePresence>
@@ -232,12 +257,11 @@ function App() {
 				value={{
 					board,
 					index,
-					actualWord,
+					actualWord: actualWord.current!,
 					login,
-					curGuess,
 					showLogin: setLogin,
+					setBoard: setBoard,
 					setIndex,
-					setGuess,
 					enterKey: keyEnter,
 					backKey: backKey,
 					letterKey: letterKey,
@@ -256,14 +280,13 @@ function App() {
 					)}
 					{stats && (
 						<StatCard
-							guessCount={guessCount}
-							userStats={userStats}
+							userStats={userStats.current}
 							stats={stats}
 							showStats={showStats}
 						></StatCard>
 					)}
 				</AnimatePresence>
-				{login && <Login userStats={userStats}></Login>}
+				{login && <Login userStats={userStats.current} ref={curGuess}></Login>}
 			</BoardContext.Provider>
 		</div>
 	);
